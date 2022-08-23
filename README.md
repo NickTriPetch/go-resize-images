0faf7ef9f67993bf266b7505199d0a6d9e419602
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2022-08-23 21:28:47 +0700

    feat: crop to center

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Set ENVs paths.
 
 > Ref: [https://jimkang.medium.com/install-go-on-mac-with-homebrew-5fa421fc55f5](https://jimkang.medium.com/install-go-on-mac-with-homebrew-5fa421fc55f5)
+
+## Resize image
+
+> Ref: [https://github.com/nfnt/resize](https://github.com/nfnt/resize)
--- src/main.go
+++ src/main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"image"
+	"image/jpeg"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nfnt/resize"
+	"github.com/oliamb/cutter"
 )
+
+func loadImageFromURL(URL string) (image.Image, error) {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, errors.New("received non 200 response code")
+	}
+
+	img, _, err := image.Decode(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
 
 func main() {
 	r := gin.Default()
@@ -12,6 +40,35 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/image", func(c *gin.Context) {
+		defaultUrl := "https://static.truck2hand.com/public/upload/item/98260/f2903f43-795c-4444-9ebe-0aaedc7877e1.jpeg"
+		url := c.DefaultQuery("url", defaultUrl)
+		rawWidth := c.DefaultQuery("w", "240")
+		width, _ := strconv.Atoi(rawWidth)
+		rawHeight := c.DefaultQuery("h", "0")
+		height, _ := strconv.Atoi(rawHeight)
+
+		image, _ := loadImageFromURL(url)
+
+		resizeImage := resize.Resize(0, uint(height), image, resize.Lanczos3)
+
+		newImage, _ := cutter.Crop(resizeImage, cutter.Config{
+			Width:  width,
+			Height: height,
+			Mode:   cutter.Centered,
+		})
+
+		buffer := new(bytes.Buffer)
+		if err := jpeg.Encode(buffer, newImage, nil); err != nil {
+			log.Println("unable to encode image.")
+		}
+
+		c.Header("Content-Type", "image/jpeg")
+		c.Header("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+
+		c.Writer.Write(buffer.Bytes())
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
